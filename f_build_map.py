8809e25761feda02ac1324dde283c652289578c0
ADAM CAMPOS <adam.campos@torc.ai>
2022-06-06 13:12:57 -0400

    add information

---
+++ f_build_map.py
@@ -0,0 +1,7 @@
+import geemap
+
+def build_map(lat, lon, zoom, vizParams, image, name):
+    map = geemap.Map(center = [lat, lon], zoom = zoom)
+    map.addLayer(image, vizParams, name)
+    
+    return map
