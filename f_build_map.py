eb32893a55c5998da2e8611aac03e905ca67f9cd
ghidora77 <ozzycampos@yahoo.com>
2022-06-08 06:30:28 -0400

    updates with photos

--- f_build_map.py
+++
@@ -1,7 +0,0 @@
-import geemap
-
-def build_map(lat, lon, zoom, vizParams, image, name):
-    map = geemap.Map(center = [lat, lon], zoom = zoom)
-    map.addLayer(image, vizParams, name)
-    
-    return map
